39148935d28f6287f464e7793764a1a5a0477eb0
Youssef Gabriel Amazirh <yg_amazirh@hotmail.ca>
2025-05-06 22:03:05 -0300

    Will do python next before finishing frontend

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+
+const app = express()
+const PORT = 5000
+
+app.use(expressLayout)
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+
+app.get('', (req,res) => {
+    res.render('partials/fileInput', {})
+})
+
+app.listen(PORT, () => {
+    console.log('App listening on port 5000')
+})
